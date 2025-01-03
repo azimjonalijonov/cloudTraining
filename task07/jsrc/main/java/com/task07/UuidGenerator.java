3044008e47659762eb0c22f3f8c756afdf54ec03
Azimjon Alijonov <azimjonalijonoff@gmail.com>
2025-01-04 00:44:04 +0500

    sdsd

--- task07/jsrc/main/java/com/task07/UuidGenerator.java
+++ task07/jsrc/main/java/com/task07/UuidGenerator.java
@@ -1,107 +1,85 @@
 package com.task07;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
 import com.syndicate.deployment.annotations.events.RuleEventSource;
-import com.syndicate.deployment.annotations.events.RuleEvents;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.*;
 
-@LambdaHandler(lambdaName = "uuid_generator",
+@LambdaHandler(
+		lambdaName = "uuid_generator",
 		roleName = "uuid_generator-role",
-		aliasName="learn",
+		isPublishVersion = true,
+		aliasName = "learn",
 		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @RuleEventSource(targetRule="uuid_trigger")
-@RuleEvents
-public class UuidGenerator implements RequestHandler<Object, Map<String, Object>> {
+@EnvironmentVariable(key = "region", value = "${region}")
+public class UuidGenerator implements RequestHandler<Object, String> {
 
-	private final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
-	private final String BUCKET_NAME = "cmtr-4d2d00ed-uuid-storage-test" ;
-	private final int NUMBER_OF_IDS_PER_FILE = 10;
-	private ObjectMapper objectMapper = new ObjectMapper();
+	private static final AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
+			.withRegion(System.getenv("region")).build();
+	private static final ObjectMapper objectMapper = new ObjectMapper();
+	private static final String S3_BUCKET = "cmtr-4d2d00ed-uuid-storage-test";
+	private static final int NUMBER_OF_IDS = 10;
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
+	public String handleRequest(Object request, Context context) {
+		LambdaLogger logger = context.getLogger();
+		logger.log("Generating UUID");
+		try
+		{
+			String fileName = String.valueOf(getNow());
+			String fileData = objectMapper.writeValueAsString(generateFileData());
 
-		System.out.println("Request: " + request);
-		System.out.println("Context: " + context);
+			putObject(fileData, fileName);
+			logger.log( "File written and uploaded to S3" );
+		}
+		catch(Exception ex)
+		{
+			logger.log("Error : "+ ex.getMessage());
+			throw new RuntimeException(ex);
+		}
+		return "success";
+	}
 
-		// Fetch Lambda execution start time
-		LocalDateTime executionStartTime = LocalDateTime.now();
+	private static void putObject(String fileData, String fileName) {
+		byte[]  contentAsBytes = fileData.getBytes(StandardCharsets.UTF_8) ;
+		ByteArrayInputStream contentsAsStream = new ByteArrayInputStream(contentAsBytes);
+		ObjectMetadata md = new ObjectMetadata();
+		md.setContentLength(contentAsBytes.length);
+		md.setContentType("application/json");
+		amazonS3.putObject(new PutObjectRequest(S3_BUCKET, fileName, contentsAsStream, md));
+	}
 
-		// Generate UUIDs
-		List<String> generatedIds = generateIds();
-		Ids ids = new Ids();
-		ids.setIds(generatedIds);
+	String getNow(){
+		return LocalDateTime.now().toString();
+	}
 
-		// Convert obj to json
-		String idsJSON = "";
-		try {
-			idsJSON = objectMapper.writeValueAsString(ids);
-		} catch (JsonProcessingException e) {
-			System.out.println("Error while parsing");
-			e.printStackTrace();
+	private Map<String, List<String>> generateFileData(){
+		Map<String, List<String>> fileData = new HashMap<>();
+		fileData.put("ids", generateUuids());
+
+		return fileData;
+	}
+
+	List<String> generateUuids(){
+		List<String> uuids = new ArrayList<>();
+		for (int i = 0; i < NUMBER_OF_IDS; i++) {
+			uuids.add(UUID.randomUUID().toString());
 		}
 
-		System.out.println(idsJSON);
-
-		// Format file name
-		String fileName = executionStartTime.toString();
-
-		try (InputStream inputStream = new ByteArrayInputStream(idsJSON.getBytes(StandardCharsets.UTF_8))) {
-			// Upload JSON content to S3
-			s3.putObject(BUCKET_NAME, fileName, inputStream, null);
-			System.out.println("JSON content has been uploaded to S3 with key: " + fileName);
-		} catch (IOException e) {
-			System.out.println("Error while uploading to S3");
-			e.printStackTrace();
-		}
-
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Successfully wrote the content in S3");
-		return resultMap;
-	}
-
-	private List<String> generateIds() {
-		return IntStream.range(0, NUMBER_OF_IDS_PER_FILE)
-				.mapToObj(i -> UUID.randomUUID().toString())
-				.collect(Collectors.toList());
-	}
-}
-
-class Ids {
-	private List<String> ids;
-
-	public List<String> getIds() {
-		return ids;
-	}
-
-	public void setIds(List<String> ids) {
-		this.ids = ids;
-	}
-
-	@Override
-	public String toString() {
-		return "Ids{" +
-				"ids=" + ids +
-				'}';
+		return uuids;
 	}
 }
